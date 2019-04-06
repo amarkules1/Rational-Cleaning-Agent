f228df5796cd79be499cd6703f36e25c502aca0a
jarhodes918 <36420589+jarhodes918@users.noreply.github.com>
2019-04-05 23:54:16 -0400

    updated with random agent

--- Agents.java
+++ Agents.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.Random;
+
 public class Agents {
 
 	public String Simple_Reflex_Agent(int location, int status) {
@@ -15,4 +17,62 @@ public class Agents {
 		
 		return "Error";
 	}	
+	
+	public void Randonmized_Simple_Reflex_Agent(int rowCt, int columnCt) {
+		
+		ScoringEnvironment env = new ScoringEnvironment(rowCt, columnCt);
+		Random r = new Random();
+
+		while(!env.isDone()) {
+			if(!env.isClean(env.get_agent_location()[0],env.get_agent_location()[1])) {
+				env.act("Suck");
+			}
+			else if(r.nextInt(4)==0) {
+				env.act("Left");
+			}
+			else if(r.nextInt(4)==1) {
+				env.act("Right");
+			}
+			else if(r.nextInt(4)==2) {
+				env.act("Up");
+			}
+			else
+				env.act("Down");				
+		}		
+	}	
+	
+	public void stateAgent(int rowCt, int columnCt) {
+		ScoringEnvironment env = new ScoringEnvironment(rowCt, columnCt);
+		int [][] visited = new int[rowCt][columnCt];
+		returnToOrigin(env);
+		while(!env.isDone()) {
+			if(!env.isClean(env.get_agent_location()[0],env.get_agent_location()[1])) {
+				env.act("Suck");
+				visited[env.get_agent_location()[0]][env.get_agent_location()[1]] = 1;
+			}
+			else if(env.get_agent_location()[1]+1<columnCt && visited[env.get_agent_location()[0]][(env.get_agent_location()[1] + 1)] !=1) {
+				visited[env.get_agent_location()[0]][env.get_agent_location()[1]] = 1;
+				env.act("Right");
+			}
+			else if(env.get_agent_location()[1]>0 && visited[env.get_agent_location()[0]][(env.get_agent_location()[1] - 1)] !=1){
+				visited[env.get_agent_location()[0]][env.get_agent_location()[1]] = 1;
+				env.act("Left");
+			}
+			else if(env.get_agent_location()[0]+1<rowCt && visited[env.get_agent_location()[0]+1][env.get_agent_location()[1]]!=1) {
+				visited[env.get_agent_location()[0]][env.get_agent_location()[1]] = 1;
+				env.act("Up");
+			}else if(!env.isDone()) {
+				returnToOrigin(env);
+			}
+		}
+		
+	}
+	public void returnToOrigin(ScoringEnvironment env) {
+		while(env.get_agent_location()[0]>0) {
+			env.act("Down");
+		}
+		while(env.get_agent_location()[1]>0) {
+			env.act("Left");
+		}
+	}
 }
--- Main.java
+++ Main.java
@@ -14,12 +14,24 @@ public class Main {
 			for (int j = 0; j < 2; j++)
 				for (int k = 0; k < 2; k++) {
 					env.set_configuration(i,j,k);
-					System.out.println("\nStart:" + i + ":" + j + ":" + k + ":" + env.get_agent_location() + ":" + (env.isClean(0,env.get_agent_location()) ? 0 : 1) + "\n");
+					System.out.println("\nStart:" + (i == 0 ? "Space A is clean" : "Space A is dirty") + ":" + 
+													(j == 0 ? "Space B is clean" : "Space B is dirty") + ":" + 
+													(k == 0 ? "Initially, Agent is in space A" : "Initially, Agent is in space B") + ":" + 
+													(env.get_agent_location() == 0 ? "Current space is A" : "Current space is B") + ":" + 
+													(env.isClean(0,env.get_agent_location()) ? "Current space status is clean" : "Current space status is dirty") + "\n");
 					for (int l = 0; l < 10; l++) {
 						action = agent.Simple_Reflex_Agent(env.get_agent_location(),env.isClean(0,env.get_agent_location()) ? 0 : 1);
 						env.act(action);
-						System.out.println(i + ":" + j + ":" + k + ":" + l + ":" + action + ":" +  env.get_agent_location() + ":" + (env.isClean(0,env.get_agent_location()) ? 0 : 1));
+						System.out.println("Step: " + (l+1) + ":" + action + ":" + 
+											(env.get_agent_location() == 0 ? "Current space is A" : "Current space is B") + ":" + 
+											(env.isClean(0,env.get_agent_location()) ? "Current space status is clean" : "Current space status is dirty"));
 					}
 				}
+				
+		
+		
+//		Agents agent2 = new Agents();
+//		agent2.Randonmized_Simple_Reflex_Agent(5,5);
+//		agent2.stateAgent(5,5);
 	}
 }
